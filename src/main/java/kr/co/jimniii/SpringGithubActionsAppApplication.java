c118a72d8d2b9b2f80e258d8af5a82bc454adcd3
jimniii <jimin7904@daum.net>
2025-09-26 10:18:50 +0900

    완료

--- src/main/java/kr/co/jimniii/SpringGithubActionsAppApplication.java
+++ src/main/java/kr/co/jimniii/SpringGithubActionsAppApplication.java
@@ -3,13 +3,11 @@ package kr.co.jimniii;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@Controller
 @SpringBootApplication
 public class SpringGithubActionsAppApplication {
 
